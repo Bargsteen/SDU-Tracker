2f88dd36ade58df8bb40afc1bb37722c573e8ea7
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2018-10-21 07:50:30 +0200

    Set up url scheme and handler, but urlevent is not triggered

--- ActivityTrackerSDU/AppDelegate.swift
+++ ActivityTrackerSDU/AppDelegate.swift
@@ -11,7 +11,7 @@ import LaunchAtLogin
 import CwlUtils
 
 @NSApplicationMain
-class AppDelegate: NSObject {
+class AppDelegate: NSObject{
     var sendOrSaveHandler : SendOrSaveHandler?
     
     override init() {
@@ -39,10 +39,35 @@ class AppDelegate: NSObject {
 extension AppDelegate: NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        UserHandler.sharedInstance.maybeAskAndSetCorrectUser()
+        
+        /*NSAppleEventManager *appleEventManager = [NSAppleEventManager sharedAppleEventManager];
+        [appleEventManager setEventHandler:self
+            andSelector:@selector(handleGetURLEvent:withReplyEvent:)
+            forEventClass:kInternetEventClass andEventID:kAEGetURL];*/
+        
+        
+        NSAppleEventManager.shared().setEventHandler(self, andSelector: #selector(AppDelegate.handleGetURLEvent(_:withReplyEvent:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
+        
+        Logging.logInfo("Current AppleEvent: \(NSAppleEventManager.shared().currentAppleEvent)")
+        Logging.logInfo("Current AppleReplyEvent: \(NSAppleEventManager.shared().currentReplyAppleEvent)")
+        //let result = LSSetDefaultHandlerForURLScheme("sdutracker" as CFString, Bundle.main.bundleIdentifier! as CFString)
+
+        //Logging.logInfo("URL RESULT: \(result)")
+        Logging.logInfo("log INFO works")
+        
+        Logging.logInfo("Current AppleEvent 222: \(NSAppleEventManager.shared().currentAppleEvent)")
+        Logging.logInfo("Current AppleReplyEvent 222: \(NSAppleEventManager.shared().currentReplyAppleEvent)")
+    
+        
+        //UserHandler.sharedInstance.maybeAskAndSetCorrectUser()
         
         let tracking = Tracking(userHandler: UserHandler.sharedInstance, sendOrSaveHandler: sendOrSaveHandler!)
         tracking.setupTracking()
+    }
+    
+    @objc func handleGetURLEvent(_ event: NSAppleEventDescriptor!, withReplyEvent:NSAppleEventDescriptor!) {
+        let url = URL(string: event.paramDescriptor(forKeyword: AEKeyword(keyDirectObject))!.stringValue!)!
+        Logging.logInfo("THE URL: " + url.absoluteString)
     }
     
     func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
